49f8225f797483d9541926042cfa078e1d424868
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-01-23 15:39:27 +0530

    Project Setup

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
+import { ChakraProvider } from '@chakra-ui/react';
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
+    <ChakraProvider>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,20 @@
+import { prisma } from './../../../lib/db/prisma';
+import NextAuth from 'next-auth';
+import Auth0Provider from 'next-auth/providers/auth0';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.JWT_SECRET,
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
+  providers: [
+    Auth0Provider({
+      id: 'auth0_provider',
+      clientId: process.env.AUTH0_CLIENT_ID || '',
+      clientSecret: process.env.AUTH0_CLIENT_SECRET || '',
+      issuer: process.env.AUTH0_ISSUER,
+    }),
+  ],
+});
